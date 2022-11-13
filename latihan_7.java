8d0a536106dbdbcd17330fd7d78c6e2f196c645c
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-13 12:04:33 +0700

    Create latihan_7.java

---
+++ latihan_7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra211022;
+/**
+ *
+ * @author HP
+ */
+public class latihan_7 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do{
+            System.out.println("Zahra Meidira");
+            i++;
+        }while(i<=100);
+    }
+}
