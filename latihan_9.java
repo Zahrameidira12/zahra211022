4203a634ae27eadec346595d374728539021433a
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-13 12:04:59 +0700

    Create latihan_9.java

---
+++ latihan_9.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra211022;
+import java.util.Scanner;
+/**
+ *
+ * @author HP
+ */
+public class latihan_9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a, b, i,pangkat = 1;
+        i = 0;
+            System.out.println("Menghitung A pangkat B ");
+            System.out.print("A\t= ");
+            a = in.nextInt();
+            System.out.print("B\t= ");
+            b = in.nextInt();
+
+            do {
+                pangkat = pangkat * a;
+                i++;
+            }while (i < b) ;
+            System.out.println("Hasil\t: ");
+            System.out.println(a + " Pangkat " + b + " = " + pangkat);
+    }
+}
